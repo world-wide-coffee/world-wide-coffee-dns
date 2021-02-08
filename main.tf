a7a7d402fceb39fa1aff443de5b821818c527a72
Fredrik Lönnblad <fredrik+github@lonnblad.com>
2021-02-08 21:47:46 +0100

    Added a developer. txt dns record set

---
+++ main.tf
@@ -0,0 +1,25 @@
+variable "google_credentials" {
+  type = string
+  description = "The Google Cloud Platform Credentials to be used to manage resources."
+}
+
+// Configure the Google Cloud provider
+provider "google" {
+  credentials = var.google_credentials
+  project     = "world-wide-coffee"
+  region      = "europe-west1"
+}
+
+data "google_dns_managed_zone" "root" {
+  name = "world-wide-coffee"
+}
+
+resource "google_dns_record_set" "developer" {
+  name = "developer.${data.google_dns_managed_zone.root.dns_name}"
+  type = "TXT"
+  ttl  = 300
+
+  managed_zone = data.google_dns_managed_zone.root.name
+
+  rrdatas = ["test"]
+}
