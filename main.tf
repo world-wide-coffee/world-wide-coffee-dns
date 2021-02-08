5091fbb528f1682bc399982142d8db6b71928457
Fredrik Lönnblad <fredrik+github@lonnblad.com>
2021-02-08 23:47:02 +0100

    Added github actions (#1)
    * Added github actions
    
    * Fixed tf linting

--- main.tf
+++ main.tf
@@ -1,5 +1,21 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+
+  backend "remote" {
+    organization = "world-wide-coffee"
+
+    workspaces {
+      name = "world-wide-coffee-dns"
+    }
+  }
+}
+
 variable "google_credentials" {
-  type = string
+  type        = string
   description = "The Google Cloud Platform Credentials to be used to manage resources."
 }
 
