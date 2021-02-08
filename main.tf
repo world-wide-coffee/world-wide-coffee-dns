cf8977a3e9b6c594e46eaf5f9178f3d0f924d59d
Fredrik Lönnblad <fredrik+github@lonnblad.com>
2021-02-08 22:14:08 +0100

    Added github actions

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+
+  backend "remote" {
+   organization = "world-wide-coffee"
+
+    workspaces {
+      name = "world-wide-coffee-dns"
+    }
+  }
+}
+
+
 variable "google_credentials" {
   type = string
   description = "The Google Cloud Platform Credentials to be used to manage resources."
