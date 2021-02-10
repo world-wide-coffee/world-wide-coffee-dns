6f8cb4185980471157b83ef26c287dbac90f61d6
Fredrik Lönnblad <fredrik+github@lonnblad.com>
2021-02-10 10:47:49 +0100

    Added the hosted zone as a resource (#3)
    * Added the hosted zone as a resource
    
    * broke main.tf into multiple files

--- main.tf
+++ main.tf
@@ -25,17 +25,3 @@ provider "google" {
   project     = "world-wide-coffee"
   region      = "europe-west1"
 }
-
-data "google_dns_managed_zone" "root" {
-  name = "world-wide-coffee"
-}
-
-resource "google_dns_record_set" "developer" {
-  name = "developer.${data.google_dns_managed_zone.root.dns_name}"
-  type = "TXT"
-  ttl  = 300
-
-  managed_zone = data.google_dns_managed_zone.root.name
-
-  rrdatas = ["test"]
-}
