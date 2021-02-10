f3c345b4d75eeb6e57cc87ea1ecf1a475010b02b
Fredrik Lönnblad <fredrik+github@lonnblad.com>
2021-02-10 09:50:47 +0100

    Added the hosted zone as a resource

--- main.tf
+++ main.tf
@@ -26,16 +26,36 @@ provider "google" {
   region      = "europe-west1"
 }
 
-data "google_dns_managed_zone" "root" {
-  name = "world-wide-coffee"
+resource "google_dns_managed_zone" "root" {
+  name     = "world-wide-coffee"
+  dns_name = "world-wide.coffee."
+
+  dnssec_config {
+    kind          = "dns#managedZoneDnsSecConfig"
+    non_existence = "nsec3"
+    state         = "on"
+
+    default_key_specs {
+      algorithm  = "rsasha256"
+      key_length = 2048
+      key_type   = "keySigning"
+      kind       = "dns#dnsKeySpec"
+    }
+    default_key_specs {
+      algorithm  = "rsasha256"
+      key_length = 1024
+      key_type   = "zoneSigning"
+      kind       = "dns#dnsKeySpec"
+    }
+  }
 }
 
 resource "google_dns_record_set" "developer" {
-  name = "developer.${data.google_dns_managed_zone.root.dns_name}"
+  name = "developer.${google_dns_managed_zone.root.dns_name}"
   type = "TXT"
   ttl  = 300
 
-  managed_zone = data.google_dns_managed_zone.root.name
+  managed_zone = google_dns_managed_zone.root.name
 
   rrdatas = ["test"]
 }
