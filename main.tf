69fa4b54272a2fc02df8c3fada9da16753bad48a
Fredrik Lönnblad <fredrik+github@lonnblad.com>
2021-02-10 10:16:38 +0100

    broke main.tf into multiple files

--- main.tf
+++ main.tf
@@ -25,37 +25,3 @@ provider "google" {
   project     = "world-wide-coffee"
   region      = "europe-west1"
 }
-
-resource "google_dns_managed_zone" "root" {
-  name     = "world-wide-coffee"
-  dns_name = "world-wide.coffee."
-
-  dnssec_config {
-    kind          = "dns#managedZoneDnsSecConfig"
-    non_existence = "nsec3"
-    state         = "on"
-
-    default_key_specs {
-      algorithm  = "rsasha256"
-      key_length = 2048
-      key_type   = "keySigning"
-      kind       = "dns#dnsKeySpec"
-    }
-    default_key_specs {
-      algorithm  = "rsasha256"
-      key_length = 1024
-      key_type   = "zoneSigning"
-      kind       = "dns#dnsKeySpec"
-    }
-  }
-}
-
-resource "google_dns_record_set" "developer" {
-  name = "developer.${google_dns_managed_zone.root.dns_name}"
-  type = "TXT"
-  ttl  = 300
-
-  managed_zone = google_dns_managed_zone.root.name
-
-  rrdatas = ["test"]
-}
