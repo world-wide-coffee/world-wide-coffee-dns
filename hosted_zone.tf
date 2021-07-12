592c3993a723736e4d7adb5f558e2c2dda1d6bdf
Fredrik Lönnblad <fredrik+github@lonnblad.com>
2021-07-12 21:07:50 +0200

    removed developer. txt record (#5)
    * removed developer. txt record
    
    * updated version attribute

--- hosted_zone.tf
+++ hosted_zone.tf
@@ -13,6 +13,7 @@ resource "google_dns_managed_zone" "root" {
       key_type   = "keySigning"
       kind       = "dns#dnsKeySpec"
     }
+
     default_key_specs {
       algorithm  = "rsasha256"
       key_length = 1024
@@ -20,14 +21,4 @@ resource "google_dns_managed_zone" "root" {
       kind       = "dns#dnsKeySpec"
     }
   }
-}
-
-resource "google_dns_record_set" "developer" {
-  name = "developer.${google_dns_managed_zone.root.dns_name}"
-  type = "TXT"
-  ttl  = 300
-
-  managed_zone = google_dns_managed_zone.root.name
-
-  rrdatas = ["test"]
 }
--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">= 1.0.2"
+
   required_providers {
     google = {
       source = "hashicorp/google"
